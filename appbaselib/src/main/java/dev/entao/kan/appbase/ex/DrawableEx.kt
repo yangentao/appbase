d16bc01ed4e3a9a55ad1c235bd13fa3308116f26
yangentao <entaoyang@163.com>
2019-10-01 02:49:52 +0800

    4.1.6

--- appbaselib/src/main/java/dev/entao/kan/appbase/ex/DrawableEx.kt
+++ appbaselib/src/main/java/dev/entao/kan/appbase/ex/DrawableEx.kt
@@ -3,15 +3,35 @@ package dev.entao.kan.appbase.ex
 import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.DrawableCompat
 
+fun Drawable.sizeW(newWidth: Int): Drawable {
+    val h = this.intrinsicHeight
+    val w = this.intrinsicWidth
+    if (newWidth == 0 || w == 0 || w == newWidth) {
+        return this
+    }
+    this.setBounds(0, 0, newWidth.dp, h * newWidth.dp / w)
+    return this
+}
+
+fun Drawable.sizeH(newHeight: Int): Drawable {
+    val h = this.intrinsicHeight
+    val w = this.intrinsicWidth
+    if (newHeight == 0 || h == 0 || h == newHeight) {
+        return this
+    }
+    this.setBounds(0, 0, w * newHeight.dp / h, newHeight.dp)
+    return this
+}
 
 fun Drawable.limited(maxEdge: Int): Drawable {
     val h = this.intrinsicHeight
     val w = this.intrinsicWidth
     if (w > maxEdge || h > maxEdge) {
+        val a = maxEdge.dp
         if (w > h) {
-            this.setBounds(0, 0, maxEdge.dp, (h * maxEdge / w).dp)
+            this.setBounds(0, 0, a, h * a / w)
         } else {
-            this.setBounds(0, 0, (w * maxEdge / h).dp, maxEdge.dp)
+            this.setBounds(0, 0, w * a / h, a)
         }
     }
     return this
