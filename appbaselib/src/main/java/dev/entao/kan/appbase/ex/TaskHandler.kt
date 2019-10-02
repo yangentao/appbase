2302ac22eeae4d6aeda2db036a56fe7e765a294c
yangentao <entaoyang@163.com>
2019-10-02 16:08:54 +0800

    418

---
+++ appbaselib/src/main/java/dev/entao/kan/appbase/ex/TaskHandler.kt
@@ -0,0 +1,32 @@
+@file:Suppress("unused", "MemberVisibilityCanBePrivate")
+
+package dev.entao.kan.appbase.ex
+
+import android.os.Handler
+import android.os.HandlerThread
+
+class TaskHandler(val name: String) {
+
+    val thread = HandlerThread(name)
+    val handler: Handler
+
+
+    init {
+        thread.start()
+        handler = Handler(thread.looper)
+    }
+
+    fun quit() {
+        handler.removeCallbacksAndMessages(null)
+        thread.quit()
+    }
+
+
+    fun post(block: () -> Unit) {
+        handler.post(block)
+    }
+
+    fun post(delay: Int, block: () -> Unit) {
+        handler.postDelayed(block, delay.toLong())
+    }
+}
